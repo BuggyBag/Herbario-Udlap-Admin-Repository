efbe62c4a3ec59cb8dab8b282090c69df707ffc2
BuggyBag <cg.estanol@ctmi.com.mx>
2026-02-27 13:56:20 -0600

    Integrate Firebase SDK for app analytics
    Added Firebase initialization and configuration script.

--- app.js
+++ app.js
@@ -82,3 +82,27 @@ function setStatus(elementId, message, error=false) {
         el.textContent = "";
     }, 3000);
 }
+
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAUwRhm44SWGefJFxgEOk31vv5VGeKPGNA",
+    authDomain: "projects-cges.firebaseapp.com",
+    projectId: "projects-cges",
+    storageBucket: "projects-cges.firebasestorage.app",
+    messagingSenderId: "620255555114",
+    appId: "1:620255555114:web:3726b649e8546f0e5f1d53",
+    measurementId: "G-87EQPE9Z8B"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
